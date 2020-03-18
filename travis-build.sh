075c6cf96f6fb142e5e722ae5d2cca258df6372d
Fabian Gruber <fabian.gruber@inria.fr>
2020-03-18 17:33:26 +0100

    Managed to get Clang+OpenMP work in Travis.

--- travis-build.sh
+++ travis-build.sh
@@ -2,24 +2,25 @@
 
 set -euo pipefail
 
-USE_OPENMP=ON
-
-if [[ "${CC:-}" == clang ]]
+if [[ "${CXX:-}" == clang++ ]]
 then
   ## Binaries produced by Clang in Travis cannot find libomp.so at runtime :/
-  USE_OPENMP=OFF
+  ## It is at /usr/local/clang*/lib/libomp.so (where '*' is the clang version)
+  ## We assume clang is installed at /usr/local/clang*/bin/clang
+  CLANG_LIB_DIR="$(dirname "$(dirname "$(which clang++)")")"/lib
+  export LD_LIBRARY_PATH="$CLANG_LIB_DIR"
 fi
 
 mkdir -p build-debug
 pushd build-debug
-cmake -DCMAKE_BUILD_TYPE=Debug -DBC_USE_OPENMP="$USE_OPENMP" ..
+cmake -DCMAKE_BUILD_TYPE=Debug ..
 VERBOSE=1 cmake --build . -- -j $(nproc)
 ctest -j $(nproc) --output-on-failure
 popd
 
 mkdir -p build-release
 pushd build-release
-cmake -DCMAKE_BUILD_TYPE=Release -DBC_USE_OPENMP="$USE_OPENMP" ..
+cmake -DCMAKE_BUILD_TYPE=Release ..
 VERBOSE=1 cmake --build . -- -j $(nproc)
 ctest -j $(nproc) --output-on-failure
 popd
